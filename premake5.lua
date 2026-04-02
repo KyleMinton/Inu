e2bc63f4f8e563f2d7da3002c7006118f7e02e43
Kyle Minton <kyle@minton.io>
2026-04-02 13:48:00 -0400

    Added GLFW to IncludeDir

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Inu"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Inu/vendor/GLFW/include"
+
+include "Inu/vendor/GLFW"
+
 project "Inu"
     location "Inu"
     kind "SharedLib"
@@ -30,7 +36,14 @@ project "Inu"
     includedirs
     {
         "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include"
+        "%{prj.name}/vendor/spdlog/include",
+        "%{IncludeDir.GLFW}"
+    }
+
+    links 
+    {
+        "GLFW",
+        "opengl32.lib"
     }
 
     filter "system:windows"
