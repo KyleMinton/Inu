7fe70a6f3616dd174ebeb905ce9f0c83b2e61ffd
Kyle Minton <kyle@minton.io>
2026-04-02 18:54:28 -0400

    Init main loop and window, add OpenGL clear and update loop

--- Inu/src/Inu/Application.cpp
+++ Inu/src/Inu/Application.cpp
@@ -1,13 +1,16 @@
 #include"inupch.h"
-
 #include "Application.h"
+
 #include "Events/ApplicationEvent.h"
 #include "Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Inu {
 
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -16,9 +19,14 @@ namespace Inu {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		INU_TRACE(e);
 
-		while (true);
+
+		while (m_Running)
+		{
+			glClearColor(0.0f, 0.7f, 0.9f, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			m_Window->OnUpdate();
+		}
 	}
 }
