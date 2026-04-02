813a5cdf3f1a089ecb78457908a3c6f2adcaec65
Kyle Minton <kyle@minton.io>
2026-04-02 18:54:48 -0400

    Add window management to Application class

--- Inu/src/Inu/Application.h
+++ Inu/src/Inu/Application.h
@@ -3,6 +3,8 @@
 
 #include"Core.h"
 #include "Events/Event.h"
+#include "../../Window.h"
+
 namespace Inu {
 
 	class INU_API Application
@@ -11,6 +13,9 @@ namespace Inu {
 		Application();
 		virtual ~Application();
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//To be defined in CLIENT
